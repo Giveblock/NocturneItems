81e66040c3c2bc9d7413b135003d4270bd24e04a
BuildTools <unconfigured@null.spigotmc.org>
2025-03-29 23:18:23 -0400

    commit

---
+++ src/main/java/me/giveblock/nocturneItems/NocturneItems.java
@@ -0,0 +1,17 @@
+package me.giveblock.nocturneItems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NocturneItems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
